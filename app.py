7efd85c8e2096d3999c650814e9add7ee7e9f9e8
Admin <307nizak@gmail.com>
2020-07-04 00:34:50 +0530

    modified

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import StandardScaler
 
 
 app = Flask(__name__)
-model = pickle.load(open('xgb_model.pkl','rb'))
+model = pickle.load(open('gbr_model.pkl','rb'))
 @app.route('/', methods = ['GET','POST'])
 def home ():
     return render_template('index.html')
